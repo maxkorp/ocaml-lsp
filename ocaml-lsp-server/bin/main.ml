153321726641906c7fa3942c90141fa3c4c6cd9d
Max Korp <maxkorp@8bytealchemy.com>
2022-05-23 12:01:33 -0700

    rename flag

--- ocaml-lsp-server/bin/main.ml
+++ ocaml-lsp-server/bin/main.ml
@@ -1,17 +1,17 @@
 let () =
   Printexc.record_backtrace true;
   let version = ref false in
-  let use_merlin_files = ref false in
+  let read_merlin_files = ref false in
   Arg.parse
     [ ("--version", Arg.Set version, "print version")
-    ; ( "--use-merlin-files"
-      , Arg.Set use_merlin_files
-      , "enables use of .merlin files" )
+    ; ( "--read-merlin-files"
+      , Arg.Set read_merlin_files
+      , "enables reading of .merlin files" )
     ]
     (fun _ -> raise (Arg.Bad "anonymous arguments are not accepted"))
     "ocamllsp";
   let version = !version in
-  let use_merlin_files = !use_merlin_files in
+  let read_merlin_files = !read_merlin_files in
   if version then
     let version = Ocaml_lsp_server.Version.get () in
     print_endline version
@@ -19,7 +19,7 @@ let () =
     let module Exn_with_backtrace = Stdune.Exn_with_backtrace in
     match
       Exn_with_backtrace.try_with (fun () ->
-          Ocaml_lsp_server.run use_merlin_files)
+          Ocaml_lsp_server.run read_merlin_files)
     with
     | Ok () -> ()
     | Error exn ->
